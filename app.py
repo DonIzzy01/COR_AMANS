f7dcb336ee7c8d9e61a927e34e1908824d8e3553
Israel Obi <isrealdegreat@gmail.com>
2026-03-16 11:42:34 -0700

    COR AMANS Cv1

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("landing.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/email-verified")
+def email_verified():
+    return render_template("email_verified.html")
+
+@app.route("/payment")
+def payment():
+    return render_template("payment.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
